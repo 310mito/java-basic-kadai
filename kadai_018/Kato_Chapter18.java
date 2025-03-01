5664ccda0756f541b6f0a0fcd668ed1adff0e2fe
310mito <a_3013nomay@i.softbank.jp>
2025-03-01 16:15:45 +0900

    kadai_018とkadai_021を追加

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,27 @@
+package kadai_018;
+
+abstract public  class Kato_Chapter18 {
+	
+	public String familyName;
+	public String givenName;
+	public String address;
+
+	
+	
+	public void commonIntroduce() {	
+		
+		this.address = "東京都中野区〇×";
+		this.familyName = "加藤";
+		System.out.println("名前は" + this.familyName + this.givenName +"です");
+		System.out.println("住所は" + this.address + "です");
+		
+	}
+//	抽象メソッド
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
